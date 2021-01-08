8200e76c0dc7e0f251ff9fb45fefa8e3d14c44a4
Michael List <ml@partnaer.com>
2021-01-08 19:50:28 +0100

    opc-ua server selection now possible again

--- src/app/browser/browser.page.ts
+++ src/app/browser/browser.page.ts
@@ -3,6 +3,8 @@ import {FlatTreeControl} from '@angular/cdk/tree';
 import {MatTreeFlatDataSource, MatTreeFlattener} from '@angular/material/tree';
 import {KonektiloBrowserService} from '../services/konektilo-browser/konektilo-browser.service';
 import {TreeDataService} from '../services/tree-data/tree-data.service';
+import {KonektiloOpcUaServer} from '../models/KonektiloOpcUaServer';
+import {Storage} from '@ionic/storage';
 
 @Component({
   selector: 'app-browser',
@@ -11,29 +13,24 @@ import {TreeDataService} from '../services/tree-data/tree-data.service';
 })
 export class BrowserPage implements OnInit {
   selectedBrowseNode: KonektiloBrowseNodeInternal;
-
-  // tslint:disable-next-line:variable-name
-  private _transformer = (node: KonektiloBrowseNodeInternal, level: number) => {
-    node.level = level;
-    return node;
-  }
+  konektiloUrl: string;
+  opcUaServer: KonektiloOpcUaServer[] = [];
+  selectedOpcUaServer: KonektiloOpcUaServer;
 
   treeControl = new FlatTreeControl<KonektiloBrowseNodeInternal>(
     node => node.level, node => node.expandable);
 
   treeFlattener = new MatTreeFlattener(
-    this._transformer, node => node.level, node => node.expandable, node => node.children);
+    (node: KonektiloBrowseNodeInternal, level: number) => {
+      node.level = level;
+      return node;
+    }
+    , node => node.level, node => node.expandable, node => node.children);
 
   dataSource = new MatTreeFlatDataSource(this.treeControl, this.treeFlattener);
 
-  constructor(public konektiloBrowser: KonektiloBrowserService, public treeDataService: TreeDataService) {
-    const opcUaTestServer = {
-      name: 'opcuatestserver',
-      browseUrl: 'http://localhost:8080/api/v1/browse/opcuatestserver',
-      comment: '',
-      online: true
-    };
-    this.treeDataService.getInitialTree(opcUaTestServer).then(initialTree => this.dataSource.data = initialTree);
+  constructor(public konektiloBrowser: KonektiloBrowserService, public treeDataService: TreeDataService, public storage: Storage) {
+    this.updateKonektiloUrl();
   }
 
   ngOnInit() {
@@ -47,5 +44,33 @@ export class BrowserPage implements OnInit {
 
   readNode(browseNode: KonektiloBrowseNodeInternal) {
     this.selectedBrowseNode = browseNode;
+  }
+
+  fetchOpcUaServer() {
+    this.opcUaServer = [];
+    this.konektiloBrowser.readOpcUaServer(this.konektiloUrl).subscribe(konektiloResponse => {
+      Object.keys(konektiloResponse.result).forEach(opcUaServer => this.opcUaServer.push(konektiloResponse.result[opcUaServer]));
+    });
+  }
+
+  updateKonektiloUrl() {
+    this.storage.ready().then(() => {
+      this.storage.get('konektiloUrl').then((konektiloUrl) => {
+        if (konektiloUrl === null) {
+          this.konektiloUrl = 'http://localhost:80';
+        } else {
+          this.konektiloUrl = konektiloUrl;
+        }
+        this.fetchOpcUaServer();
+      });
+    });
+  }
+
+  selectOpcUaServer() {
+    this.treeDataService.getInitialTree(this.selectedOpcUaServer).then(initialTree => this.dataSource.data = initialTree);
+  }
+
+  compareOpcUaServer(s1: KonektiloOpcUaServer, s2: KonektiloOpcUaServer): boolean {
+    return s1.name === s2.name;
   }
 }
