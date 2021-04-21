ee5cff508951a4f6f5522e1913596f0dd28f91fc
Michael List <ml@partnaer.com>
2021-04-21 17:14:09 +0200

    Throw error message on home.page.ts if could not connect to konektilo

--- src/pages/home/home.page.ts
+++ src/pages/home/home.page.ts
@@ -1,8 +1,9 @@
 import {Component} from '@angular/core';
 import {KonektiloOpcUaServer} from '../../models/KonektiloOpcUaServer';
 import {KonektiloBrowserService} from '../../services/konektilo-browser/konektilo-browser.service';
-import {KonektiloService} from '../../services/konektilo/konektilo.service';
 import {SettingsStorageService} from '../../services/settings-storage/settings-storage.service';
+import {ToastController} from '@ionic/angular';
+import {catchError} from 'rxjs/operators';
 
 @Component({
   selector: 'app-home',
@@ -11,6 +12,7 @@ import {SettingsStorageService} from '../../services/settings-storage/settings-s
 })
 export class HomePage {
   opcUaServer: KonektiloOpcUaServer[] = [];
+  connectionSuccessful = false;
 
   konektiloUrlInput: string;
   konektiloUserInput: string;
@@ -18,7 +20,8 @@ export class HomePage {
 
   konektiloSettings: KonektiloSettings;
 
-  constructor(public konektiloBrowser: KonektiloBrowserService, public konektilo: KonektiloService,
+  constructor(public toastController: ToastController,
+              public konektiloBrowser: KonektiloBrowserService,
               public settingsStorageService: SettingsStorageService) {
     this.settingsStorageService.getSettings().then(konektiloSettings => {
       this.konektiloSettings = konektiloSettings;
@@ -36,13 +39,37 @@ export class HomePage {
     this.konektiloSettings.user = this.konektiloUserInput;
     this.konektiloSettings.password = this.konektiloPasswordInput;
     await this.settingsStorageService.saveSettings(this.konektiloSettings);
+
+    this.connectToKonektilo();
   }
 
   connectToKonektilo() {
-    // TODO show user if connection ok
     this.opcUaServer = [];
-    this.konektiloBrowser.readOpcUaServer(this.konektiloUrlInput).subscribe(konektiloResponse => {
+
+    // @ts-ignore
+    this.konektiloBrowser.readOpcUaServer(this.konektiloUrlInput).pipe(catchError(error => {
+      this.showConnectionStatus(false, error?.statusText).then();
+    })).subscribe(konektiloResponse => {
+      this.connectionSuccessful = true;
+      // @ts-ignore
       Object.keys(konektiloResponse.result).forEach(opcuaServer => this.opcUaServer.push(konektiloResponse.result[opcuaServer]));
+      this.showConnectionStatus(true, '').then();
     });
+  }
+
+  async showConnectionStatus(successful: boolean, errorMessage: string) {
+    let message;
+
+    if (successful) {
+      message = 'Connected successfully, found ' + this.opcUaServer.length + ' OPC-UA server(s)';
+    } else {
+      message = 'Could not connect to konektilo: ' + errorMessage;
+    }
+
+    const toast = await this.toastController.create({
+      message,
+      duration: 2000
+    });
+    await toast.present();
   }
 }
